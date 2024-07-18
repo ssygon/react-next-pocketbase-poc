ccb4ea3493b79728209479de14935d241d0bfe42
Shaun Ong <ssygon@gmail.com>
2024-07-18 17:48:55 +0800

    refactor and styling changes

---
+++ app/components/CreateNote/CreateNote.tsx
@@ -0,0 +1,78 @@
+'use client';
+
+import { config } from '../../../config';
+import PocketBase from "pocketbase";
+import { useState } from 'react';
+// import { useRouter } from 'next/navigation';
+
+const CreateNote = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  // const router = useRouter();
+
+  const create = async () => {
+    try {
+      const db = new PocketBase(config.pocketbaseAPIBaseUrl);
+
+      const record = await db.collection('notes').create({
+        title,
+        content,
+      });
+
+      setContent('');
+      setTitle('');
+
+      closeDialog();
+
+      // router.refresh();
+    } catch (error) {
+      console.error('Error creating a note:', error);
+    }
+  }
+
+  const closeDialog = () => {
+    setDialogOpen(false);
+  }
+
+  const toggleDialog = () => {
+    setDialogOpen(!dialogOpen);
+  }
+
+  return (
+    <>
+      <button className="btn" onClick={toggleDialog}>Add a Note</button>
+      {dialogOpen && (
+        <div className="dialog">
+          <form onSubmit={create}>
+            <div className="flex flex-col px-4 py-3">
+              <h3>Add a Note</h3>
+              <input
+                type="text"
+                placeholder="Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+              <textarea
+                placeholder="Content"
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+              />
+            </div>
+            <div className="px-4 py-3 sm:flex sm:justify-end sm:px-6">
+              <button className="btn" type="submit">
+                Create note
+              </button>
+              <button className="btn-cancel mt-3 sm:ml-3 sm:mt-0" type="button" onClick={closeDialog}>
+                Cancel
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default CreateNote;
--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,46 +1,51 @@
+import { config } from '../../config';
 import PocketBase from "pocketbase";
 import Note from "../components/Note/Note";
+import CreateNote from "../components/CreateNote/CreateNote";
 
-async function getNotes() {
-    try {
-        // const res = await fetch('http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30', { cache: 'no-store' });  // no-store is important
-        // const data = await res.json();
-        const db = new PocketBase('http://127.0.0.1:8090');
-        const data = await db.collection('notes').getList(1, 30, { cache: 'no-store' });
+const getNotes = async () => {
+  try {
+    // const res = await fetch(`{config.pocketbaseAPIBaseUrl}/api/collections/notes/records?page=1&perPage=30`, { cache: 'no-store' });  // no-store is important
+    // const data = await res.json();
+    const db = new PocketBase(config.pocketbaseAPIBaseUrl);
+    const data = await db.collection('notes').getList(1, 30, { cache: 'no-store' });
 
-        console.log(data);
+    console.log(data);
 
-        return data?.items || [];
-    } catch (error) {
-        console.error('Error fetching notes:', error);
-        return [];
-    }
+    return data?.items || [];
+  } catch (error) {
+    console.error('Error fetching notes:', error);
+    return [];
+  }
 }
 
+const NotesPage = async () => {
+  const notes = await getNotes();
 
-export default async function NotesPage() {
-    const notes = await getNotes();
-
-    return(
-      <>
-        <h1>Notes Page</h1>
-        {notes.length === 0 ? (
-          <>
-            <p>
-              Please run in terminal: <code>$ ./pocketbase serve</code> to start the pocketbase db server.
-            </p>
-            <p>
-              Then refresh page to see the list of notes.
-            </p>
-          </>
-        ) : (
-          <div className="flex flex-wrap gap-4">
-              {notes.map((note) => (
-                  <Note key={note.id} id={note.id} title={note?.title} content={note?.content}/>
-              ))}
-          </div>
-        )}
-      </>
-    );
+  return(
+    <>
+      <h1>Notes Page</h1>
+      {notes.length === 0 ? (
+        <>
+          <p>
+            Please run in terminal: <code>$ ./pocketbase serve</code> to start the pocketbase db server.
+          </p>
+          <p>
+            Then refresh page to see the list of notes.
+          </p>
+        </>
+      ) : (
+        <>
+        <CreateNote />
+        <div className="grid gap-x-4 gap-y-4 grid-cols-2 mt-4 sm:grid-cols-3 md:grid-cols-4">
+          {notes.map((note) => (
+              <Note key={note.id} id={note.id} title={note?.title} content={note?.content}/>
+          ))}
+        </div>
+        </>
+      )}
+    </>
+  );
 }
 
+export default NotesPage;
